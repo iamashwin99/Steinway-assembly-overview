2a1f4613bc06a283163345f84c892aaef049701c
iamashwin99 <ashwin-kumar.karnad@mpsd.mpg.de>
2024-02-23 09:47:29 +0100

    add demo tables

--- main.py
+++ main.py
@@ -7,20 +7,33 @@ Has two views, one for the floor overview and one for the piano production stage
 
 import streamlit as st
 from streamlit_option_menu import option_menu
+import pandas as pd
 
 
-DEMO_FLOORS = ["Floor 1", "Floor 2", "Floor 3", "Floor 4", "Floor 5"]
+
+def get_sheet_df(sheet_name):
+    """
+    Get a df from a google sheet
+
+    """
+    sheet_id = "1ETxOtf51_kOt5qVp2d-5wP7ihQMNgNe-qydSm3aVLJA"
+    # sheet_name = "HH-M-135"
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+    piano_df = pd.read_csv(url)
+    return piano_df
+
+DEMO_FLOORS = ["Floor1", "Floor2", "Floor3", "Floor4", "Floor5"]
 DEMO_SERIAL_NO = [
-    "St-M-135",
-    "St-D-235",
-    "St-M-203",
-    "St-D-456",
-    "St-M-789",
-    "St-D-987",
-    "St-M-246",
-    "St-D-789",
-    "St-M-357",
-    "St-D-159",
+    "HH-M-135",
+    "NY-D-235",
+    "HH-M-203",
+    "NY-D-456",
+    "HH-M-789",
+    "NY-D-987",
+    "HH-M-246",
+    "NY-D-789",
+    "HH-M-357",
+    "NY-D-159",
 ]
 DEMO_SERIAL_NO.sort()
 # wide webapp with sidebar closed.P
@@ -44,8 +57,13 @@ if selected_view == "Floor view":
     # add a dropdown to select the floor
     selected_floor = st.selectbox("Select floor", DEMO_FLOORS)
     st.write(f"Showing production for {selected_floor}")
+    floor_df = get_sheet_df(selected_floor)
+    st.write(floor_df.head())
 
 else:
     st.write("## Piano view")
     # add a searchable dropdown to select the piano serial number
     seleted_piano = st.selectbox("Select piano serial number", DEMO_SERIAL_NO)
+    st.write(f"Showing production for {seleted_piano}")
+    floor_df = get_sheet_df(seleted_piano)
+    st.write(floor_df.head())
